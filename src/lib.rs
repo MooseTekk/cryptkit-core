ea99b1a60eb107fa2f0e9c13a02368a840834aed
Moose Tekk <moosetekk@gmail.com>
2022-08-01 14:53:20 +1000

    ascii85

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
-
-
 use wasm_bindgen::prelude::*;
+use std::str;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn ascii85_encode(input: &str) -> String {
+    ascii85::encode(input.as_bytes())
+}
+#[wasm_bindgen]
+pub fn ascii85_decode(input: &str) -> String {
+    str::from_utf8(
+        &ascii85::decode(input).unwrap()
+    ).unwrap_or("Invalid Input").to_string()
 }
